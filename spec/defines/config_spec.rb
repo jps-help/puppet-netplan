65aeb7429b0f73b0371e3fc6ed896141bc7f6276
James Paton-Smith <jamesps@ebi.ac.uk>
2024-03-18 14:40:31 +0000

    Update spec tests
    Add comprehensive tests for init.pp
    Add comprehensive tests for config.pp

--- spec/defines/config_spec.rb
+++ spec/defines/config_spec.rb
@@ -3,7 +3,8 @@
 require 'spec_helper'
 
 describe 'netplan::config' do
-  let(:title) { 'namevar' }
+  let(:title) { 'default' }
+  let(:pre_condition) { 'include netplan'}
   let(:params) do
     {}
   end
@@ -13,6 +14,27 @@ describe 'netplan::config' do
       let(:facts) { os_facts }
 
       it { is_expected.to compile }
+
+      context 'simple_config' do
+        let(:title) { 'simple_config' }
+        let(:params) do
+          super().merge({
+            'ensure' => 'present',
+            'priority' => 20,
+            'settings' => {
+              'version' => 2,
+              'renderer' => 'networkd',
+              'ethernets' => {
+                'eth0' => {
+                  'dhcp4' => true,
+                },
+              },
+            }
+          })
+        end
+        it { is_expected.to contain_file("/etc/netplan/#{params['priority']}-#{title}.yaml").with_mode('0600') }
+        it { is_expected.to contain_file("/etc/netplan/#{params['priority']}-#{title}.yaml").that_notifies('Exec[netplan_cmd]')}
+      end
     end
   end
 end
